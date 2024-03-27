0b3a4f3c5ab086145551ebac5171b5c220928ad4
govind2522 <100400510+govind2522@users.noreply.github.com>
2024-03-27 16:35:38 +0530

    Added image optimization code

--- src/main/java/com/function/java/ThumbnailFunction.java
+++ src/main/java/com/function/java/ThumbnailFunction.java
@@ -1,12 +1,17 @@
 package com.function.java;
 
+import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.Iterator;
 import java.util.logging.Logger;
+import javax.imageio.IIOImage;
 import javax.imageio.ImageIO;
+import javax.imageio.ImageWriteParam;
 import javax.imageio.ImageWriter;
+import javax.imageio.stream.ImageOutputStream;
 import com.azure.core.http.rest.Response;
 import com.azure.core.util.BinaryData;
 import com.azure.core.util.Context;
@@ -54,23 +59,71 @@ public class ThumbnailFunction {
       logger.info("file is downloaded here");
 
       // Logic for image optimization
+      InputStream optimizeImage = optimizeImage(imgFile, fileName, logger);
+      long optimizedImageContentLength = getOptimizedImageContentLength(optimizeImage);
+      logger.info("optimizedImageContentLength :: " + optimizedImageContentLength);
 
-      // get file extension
-      String ext = getFileExtension(fileName);
-      logger.info("file extension is :: " + ext);
-
-      Iterator<ImageWriter> writers = ImageIO.getImageWritersByFormatName(ext);
-      ImageWriter writer = writers.next();
-
-      // uploading file
+      // uploading file logic
       String thumbContainer = System.getenv("THUMB_CONTAINER");
-      storeFile(fileName, imgFile, Long.valueOf(contentLength), contentType, imgConnectionStr, thumbContainer, logger);
-
-      logger.info("file is Uploaded in " + thumbContainer + " container :: " + ext);
+      storeFile(fileName, optimizeImage, optimizedImageContentLength, contentType, imgConnectionStr, thumbContainer,
+          logger);
 
     } catch (Exception e) {
       logger.severe("Error processing event: " + e.getMessage());
     }
+  }
+
+  public InputStream optimizeImage(InputStream imageInputStream, String fileName, Logger logger) throws IOException {
+    // Read the image from the input stream
+    BufferedImage image = ImageIO.read(imageInputStream);
+
+    // get file extension
+    String ext = getFileExtension(fileName);
+    logger.info("file extension is :: " + ext);
+
+    String compressionQuality = System.getenv("COMPRESSION_QUALITY"); // 0.75f
+
+    // Get the image writer for the JPEG format
+    Iterator<ImageWriter> writers = ImageIO.getImageWritersByFormatName(ext);
+    ImageWriter writer = writers.next();
+
+    // Create a new image output stream to store the optimized image
+    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    ImageOutputStream ios = ImageIO.createImageOutputStream(baos);
+
+    // Set the image write parameters to optimize the image quality
+    ImageWriteParam writeParam = writer.getDefaultWriteParam();
+    writeParam.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
+    writeParam.setCompressionQuality(Float.valueOf(compressionQuality)); // Adjust the quality setting as needed
+
+    // Write the optimized image to the output stream
+    writer.setOutput(ios);
+    writer.write(null, new IIOImage(image, null, null), writeParam);
+
+    // Close the image writer and output stream
+    writer.dispose();
+    ios.close();
+
+    logger.info("Image is optimized.");
+    // Return the optimized image as an input stream
+    return new ByteArrayInputStream(baos.toByteArray());
+  }
+
+  public long getOptimizedImageContentLength(InputStream imageStream) throws IOException {
+    // Get the content length of the optimized image
+    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    byte[] buffer = new byte[1024];
+    int bytesRead;
+    while ((bytesRead = imageStream.read(buffer)) != -1) {
+      baos.write(buffer, 0, bytesRead);
+    }
+    long contentLength = baos.size();
+
+    // Close the output stream
+    baos.close();
+
+    // Return the content length
+    return contentLength;
   }
 
   private String getFileExtension(String fileName) {
