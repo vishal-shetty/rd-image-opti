8689e31362ca40784ee8df47faba80c0001f7e98
govind2522 <100400510+govind2522@users.noreply.github.com>
2024-03-27 14:54:43 +0530

    added upload logic

--- src/main/java/com/function/java/ThumbnailFunction.java
+++ src/main/java/com/function/java/ThumbnailFunction.java
@@ -1,11 +1,23 @@
 package com.function.java;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.util.Iterator;
 import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+import javax.imageio.ImageWriter;
+import com.azure.core.http.rest.Response;
+import com.azure.core.util.BinaryData;
+import com.azure.core.util.Context;
 import com.azure.storage.blob.BlobClient;
 import com.azure.storage.blob.BlobContainerClient;
 import com.azure.storage.blob.BlobServiceClient;
 import com.azure.storage.blob.BlobServiceClientBuilder;
+import com.azure.storage.blob.models.BlobHttpHeaders;
+import com.azure.storage.blob.models.BlobRequestConditions;
+import com.azure.storage.blob.models.BlockBlobItem;
+import com.azure.storage.blob.options.BlobParallelUploadOptions;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.microsoft.azure.functions.ExecutionContext;
@@ -38,22 +50,47 @@ public class ThumbnailFunction {
       String imgConnectionStr = System.getenv("AzureWebJobsStorage");
       String container = System.getenv("IMAGE_CONTAINER");
       logger.info("images connection string :: " + imgConnectionStr + " container :: " + container);
-      String downloadFile = downloadFile(fileName, container, imgConnectionStr, logger);
-      logger.info("file is downloaded here :: " + downloadFile);
+      InputStream imgFile = downloadFile(fileName, container, imgConnectionStr, logger);
+      logger.info("file is downloaded here");
+
+      // Logic for image optimization
+
+      // get file extension
+      String ext = getFileExtension(fileName);
+      logger.info("file extension is :: " + ext);
+
+      Iterator<ImageWriter> writers = ImageIO.getImageWritersByFormatName(ext);
+      ImageWriter writer = writers.next();
+
+      // uploading file
+      String thumbContainer = System.getenv("THUMB_CONTAINER");
+      storeFile(fileName, imgFile, Long.valueOf(contentLength), contentType, imgConnectionStr, thumbContainer, logger);
+
+      logger.info("file is Uploaded in " + thumbContainer + " container :: " + ext);
 
     } catch (Exception e) {
       logger.severe("Error processing event: " + e.getMessage());
     }
   }
 
-  public String downloadFile(String blobitem, String containerName, String connectionstring, Logger logger) {
+  private String getFileExtension(String fileName) {
+    String extension = null;
+    int i = fileName.lastIndexOf('.');
+    if (i > 0) {
+      extension = fileName.substring(i + 1);
+    }
+    return extension;
+  }
+
+  public InputStream downloadFile(String blobitem, String containerName, String connectionstring, Logger logger) {
     logger.info("creating connection");
     BlobContainerClient containerClient = containerClient(connectionstring, containerName, logger);
     BlobClient blobClient = containerClient.getBlobClient(blobitem);
     ByteArrayOutputStream os = new ByteArrayOutputStream();
     blobClient.downloadStream(os);
-    logger.info("returning os");
-    return os.toString();
+    InputStream is = new ByteArrayInputStream(os.toByteArray());
+    logger.info("returning input stream");
+    return is;
   }
 
   private BlobContainerClient containerClient(String connectionstring, String containerName, Logger logger) {
@@ -63,4 +100,23 @@ public class ThumbnailFunction {
     logger.info("got blob client here");
     return blobContainer;
   }
+
+  public void storeFile(String filename, InputStream content, long length, String contentType,
+      String connectionstring, String containerName, Logger logger) {
+    logger.info("Azure store file BEGIN " + filename);
+    BlobClient client = containerClient(connectionstring, containerName, logger).getBlobClient(filename);
+
+    BlobHttpHeaders jsonHeaders = new BlobHttpHeaders().setContentType(contentType);
+    BinaryData data = BinaryData.fromStream(content, length);
+    BlobParallelUploadOptions options =
+        new BlobParallelUploadOptions(data).setRequestConditions(new BlobRequestConditions()).setHeaders(jsonHeaders);
+    Response<BlockBlobItem> uploadWithResponse = client.uploadWithResponse(options, null, Context.NONE);
+    uploadWithResponse.getRequest().getUrl();
+
+    logger.info("Azure store file END with status code" + uploadWithResponse.getStatusCode());
+    if (uploadWithResponse.getStatusCode() != 201) {
+      logger.severe("Error uploading file : " + uploadWithResponse);
+    }
+  }
+
 }
