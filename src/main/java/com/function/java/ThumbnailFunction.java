70213bac3dfedd0d04d4c7bfb1537e1a2325f637
govind2522 <100400510+govind2522@users.noreply.github.com>
2024-03-27 11:00:31 +0530

    Added logs for blob details

--- src/main/java/com/function/java/ThumbnailFunction.java
+++ src/main/java/com/function/java/ThumbnailFunction.java
@@ -1,40 +1,38 @@
 package com.function.java;
 
-import com.microsoft.azure.eventgrid.models.EventGridEvent;
+import java.util.logging.Logger;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.microsoft.azure.functions.ExecutionContext;
-import com.microsoft.azure.functions.annotation.*;
+import com.microsoft.azure.functions.annotation.EventGridTrigger;
+import com.microsoft.azure.functions.annotation.FunctionName;
 
 public class ThumbnailFunction {
+  
+  @FunctionName("BlobTriggerFunction")
+  public void run(@EventGridTrigger(name = "event") String event, final ExecutionContext context) {
+    Logger logger = context.getLogger();
+    try {
 
-  	@FunctionName("BlobTriggerFunction")
-    public void run(
-        @EventGridTrigger(name = "event") String event,
-        @BlobInput(
-            name = "inputBlob",
-            dataType = "binary",
-            connection = "AzureWebJobsStorage",
-            path = "northshirecc-qa-images/{name}"
-        ) byte[] content,
-        @BindingName("name") String blobName, // Dynamically extracted blob name
-        final ExecutionContext context
-    ) {
-        try {
-            // Parse the event data
-            // You can use libraries like Jackson or Gson to deserialize the JSON event.
-            // Extract container and blob names from the event data.
-            context.getLogger().info("Blob uploaded: " + event);
+      ObjectMapper mapper = new ObjectMapper();
+      logger.info("Blob uploaded: " + event);
+      logger.info("Hello world Java...");
+      
+      // Parse the event data
+      JsonNode eventNode = mapper.readTree(event);
+      JsonNode dataNode = eventNode.get("data");
+      String imageUrl = dataNode.get("url").asText();
+      String contentType = dataNode.get("contentType").asText();
+      String contentLength = dataNode.get("contentLength").asText();
+      
+      // logging details
+      logger.info("Uploaded Image URL :: " + imageUrl);
+      logger.info("Uploaded Image ContentType :: " + contentType);
+      logger.info("Uploaded Image contentLength :: " + contentLength);
 
-            // Process the blob content (e.g., read, transform, etc.)
-            // You can access the blob content via the 'content' parameter.
-            // ...
-
-            // You can also call other services, write to a database, etc.
-            // ...
-            context.getLogger().info("Blob name: " + blobName); // Dynamically extracted blob name
-
-        } catch (Exception e) {
-            context.getLogger().severe("Error processing event: " + e.getMessage());
-        }
-    } 
-
+    } catch (Exception e) {
+      logger.severe("Error processing event: " + e.getMessage());
+    }
+  }
 }
+
