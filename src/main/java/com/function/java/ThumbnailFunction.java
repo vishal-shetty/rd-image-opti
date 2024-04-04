2651a3afed200f8d13236e844e882d2452a4874b
govind2522 <100400510+govind2522@users.noreply.github.com>
2024-04-04 17:15:18 +0530

    Updated valid extension for img

--- src/main/java/com/function/java/ThumbnailFunction.java
+++ src/main/java/com/function/java/ThumbnailFunction.java
@@ -45,7 +45,7 @@ public class ThumbnailFunction {
   @FunctionName("BlobTriggerFunction")
   public void run(@EventGridTrigger(name = "event") String event, final ExecutionContext context) {
     Logger logger = context.getLogger();
-    final List<String> validImgExt = Arrays.asList("JPG", "JPEG", "PNG");
+    final List<String> validImgExt = Arrays.asList("JPG", "JPEG", "PNG", "JFIF", "TIFF", "TIF", "BMP");
     final List<String> validVdoExt = Arrays.asList("MP4", "MOV", "3GPP", "3GP");
     try {
 
